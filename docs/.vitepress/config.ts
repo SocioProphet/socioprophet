9f74a26ba9ad6629203f209cda3e14ce4eee846c
Michael Heller <21163552+mdheller@users.noreply.github.com>
2026-02-17 14:56:56 -0500

    chore: compile Tailwind for marketing, add docs GA, add deploy script

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,11 @@ const repo = (process.env.GITHUB_REPOSITORY || "socioprophet").split("/").pop() 
 const base = process.env.DOCS_BASE || "/" + repo + "/";
 
 export default defineConfig({
-  head: [["link", { rel: "icon", href: "/icon.png" }]],
+  head: [
+    ["link", { rel: "icon", href: "/icon.png" }],
+    ["script", { async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-GL5Y2786Z6" }],
+    ["script", {}, "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\"js\", new Date()); gtag(\"config\", \"G-GL5Y2786Z6\"); function spTrack(name, params){ try{ if(typeof gtag===\"function\") gtag(\"event\", name, params||{}); }catch(e){} }"],
+  ],
   title: "SocioProphet",
   description: "SocioProphet documentation",
   base,
